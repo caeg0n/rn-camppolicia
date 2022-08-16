164fa26e091836caea9b7b62a57f53271cff820b
caegon <caegon2005@hotmail.com>
2022-08-16 19:45:45 -0300

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,19 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { persistCombineReducers } from 'redux-persist';
 import thunk from 'redux-thunk';
 import userReducer from './reducers';
-const rootReducer = combineReducers({userReducer}); 
-export const Store = createStore(rootReducer, applyMiddleware(thunk));
+import { persistStore, persistReducer } from 'redux-persist'
+
+const rootReducer = combineReducers({userReducer});
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  timeout: null
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+//export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const Store = createStore(persistedReducer, applyMiddleware(thunk))
+export const persistor = persistStore(Store)
